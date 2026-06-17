c217962f367594d8fdccd54013594eaafbc02e28
Eyy39 <phonsovaney8@gmail.com>
2026-06-17 13:38:25 +0700

    update

--- project/db/database.js
+++ project/db/database.js
@@ -1,10 +1,10 @@
-const { Sequelize } = require('sequelize');
+const { Sequelize } = require("sequelize");
 
 const sequelize = process.env.DATABASE_URL
   ? new Sequelize(process.env.DATABASE_URL, { logging: false })
   : new Sequelize({
-      dialect: 'sqlite',
-      storage: ':memory:',
+      dialect: "sqlite",
+      storage: ":memory:",
       logging: false,
     });
 
