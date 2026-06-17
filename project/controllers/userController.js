1c937e66adea48e1ac35a721698cdbfaed8a7351
Vicheka <andronikaalison@gmail.com>
2026-06-17 13:21:54 +0700

    create structure

---
+++ project/controllers/userController.js
@@ -0,0 +1,20 @@
+// Handles business logic for users
+const { User } = require('../model/user');
+
+exports.getAllUsers = async (req, res) => {
+  try {
+    const users = await User.findAll();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ error: 'Server error' });
+  }
+};
+
+exports.createUser = async (req, res) => {
+  try {
+    const user = await User.create(req.body);
+    res.status(201).json(user);
+  } catch (err) {
+    res.status(400).json({ error: 'Invalid data' });
+  }
+};
